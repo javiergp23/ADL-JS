e9e1b8b7f3a96fefcec3f4e07f6a34be353b64da
Javier Gonzalez <javgon2306@gmail.com>
2024-07-02 19:24:04 -0300

    first-commit

---
+++ assets/JS/script.js
@@ -0,0 +1,10 @@
+let nombre = prompt("¿Cual es tu nombre?");
+let apellido = prompt("¿Cual es tu Apellido?");
+let edad = prompt("¿Cual es tu edad?");
+let deporteFav = prompt("¿Cual es tu deporte favorito?");
+
+
+document.write("Nombre: " + nombre + "<br/>");
+document.write("Apellido: " + apellido + "<br/>");
+document.write("Edad: " + edad + "<br/>");
+document.write("Deporte Favorito: " + deporteFav + "<br/>");
